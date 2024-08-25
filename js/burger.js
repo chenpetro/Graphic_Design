82d548c806cf3d3087d20ab92b767f5d78a6d24c
chenpetro <chenpetro@gmail.com>
2024-08-25 14:28:17 +0200

    modal done

--- js/burger.js
+++ js/burger.js
@@ -1,6 +1,7 @@
 const burger = document.querySelector(".burger");
 const menu = document.querySelector(".menu-hidden");
 const close = document.querySelector(".menu-hidden__close");
+const backdrop = document.querySelector(".backdrop-menu");
 
 
 let i = 1;
@@ -8,11 +9,16 @@ burger.addEventListener("click", function () {
   i++;
   console.log(i);
   if (i % 2 == 0) {
+    backdrop.style.display = "flex";
     menu.style.display = "flex";
+    document.body.classList.toggle("no-scroll");
   } else {
     menu.style.display = "none";
+    backdrop.style.display = "none";
   }
 });
 close.addEventListener("click", function () {
     menu.style.display = "none";
+    backdrop.style.display = "none";
+    document.body.classList.toggle("no-scroll");
 })
---
+++ js/modal.js
@@ -0,0 +1,27 @@
+const backdr = document.querySelector(".backdrop");
+const contactme = document.querySelector(".header__right-button");
+const modalsend = document.querySelector(".modal-send");
+const thankyou = document.querySelector(".thank-you");
+const thx = document.querySelector(".thx");
+const cls = document.querySelector(".cls");
+
+contactme.addEventListener("click", function () {
+  backdr.style.display = "block";
+  document.body.classList.toggle("no-scroll");
+})
+
+modalsend.addEventListener("click", function () {
+  backdr.style.display = "block";
+  backdr.style.zIndex = "16";
+  thankyou.style.display = "block";
+  thx.style.display = "block";
+  cls.style.display = "block";
+})
+
+cls.addEventListener("click", function () {
+  backdr.style.display = "none";
+  thankyou.style.display = "none";
+  thx.style.display = "none";
+  cls.style.display = "none";
+  document.body.classList.toggle("no-scroll");
+})
